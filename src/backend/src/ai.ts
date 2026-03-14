916e74536bbc7d4100139aab8228b785235c526e
starsandnovas <starsandnovasidk@gmail.com>
2026-03-14 17:24:33 +1100

    fix(architecture): Move global constant schema and interfaces into a interfaces.ts file

--- src/backend/src/ai.ts
+++ src/backend/src/ai.ts
@@ -1,6 +1,9 @@
 import { GoogleGenAI } from "@google/genai";
-import z from 'zod';
 import dotenv from "dotenv";
+import {
+  RecipeRequest,
+  listRecipeSchema
+} from './interfaces.ts'
 
 // Loads API key from .env
 dotenv.config();
@@ -9,28 +12,6 @@ dotenv.config();
 const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY
 });
-
-interface RecipeRequest {
-  ingredients: { name: string, amount: string }[]
-}
-
-// Enforces a schema
-const ingredientSchema = z.object({
-  name: z.string(),
-  quantity: z.string()
-});
-
-const recipeSchema = z.object({
-  'name': z.string(),
-  'imageDescription': z.string(),
-  'description': z.string(),
-  'ingredients': z.array(ingredientSchema),
-  'cookTime': z.string(),
-  'serves': z.string(),
-  'steps': z.array(z.string())
-});
-
-const listRecipeSchema = z.array(recipeSchema)
 
 // responseMimeType enforces a JSON response
 export async function generateRecipe(req: RecipeRequest) {
--- src/backend/src/interfaces.ts
+++ src/backend/src/interfaces.ts
@@ -1,3 +1,23 @@
-interface RecipeRequest {
-  ingredients: string[];
+import z from 'zod';
+
+export interface RecipeRequest {
+  ingredients: { name: string, amount: string }[]
 }
+
+// Enforces a schema
+export const ingredientSchema = z.object({
+  name: z.string(),
+  quantity: z.string()
+});
+
+export const recipeSchema = z.object({
+  'name': z.string(),
+  'imageDescription': z.string(),
+  'description': z.string(),
+  'ingredients': z.array(ingredientSchema),
+  'cookTime': z.string(),
+  'serves': z.string(),
+  'steps': z.array(z.string())
+});
+
+export const listRecipeSchema = z.array(recipeSchema)
