ab9c095316a7c5fd76ed3e82aa7de075cb24357c
Nicole <z5677783@ad.unsw.edu.au>
2026-03-15 13:14:28 +1100

    feat(frontend): add illustration to landing page

--- src/frontend/budget-fridge/src/components/home/LeftPanel.tsx
+++ src/frontend/budget-fridge/src/components/home/LeftPanel.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { InputGroup } from "./InputGroup";
 import { useRecipe } from '../../context/RecipeContext';
+import wok from '../../assets/wok.svg';
 
 export default function LeftPanel() {
   const { addIngredient } = useRecipe();
@@ -16,7 +17,8 @@ export default function LeftPanel() {
 
   return (
     <div className="flex flex-col justify-center items-center text-center px-[60px] py-[80px]">
-      <h1 className="font-heading text-[72px] font-bold leading-[0.95] tracking-[-1px] text-text mb-3 py-4">
+      <img src={wok} alt="wok" width={325} height={325} className="-mb-4" />
+      <h1 className="font-heading text-[64px] font-bold leading-[0.95] tracking-[-1px] text-text mb-4 pt-0 pb-4">
         What's in<br />your fridge?
       </h1>
       <InputGroup
